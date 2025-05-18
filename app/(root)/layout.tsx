3a50f2ede5acfeb7c9434a42c90e8600f5fbdfdf
Mohammed khaleelullah <khaleelullah@edvoy.com>
2025-05-18 19:21:13 +0530

    Added analytics in vercel

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,12 @@
 import {ReactNode} from "react";
 import Navbar from "@/components/Navbar";
-
+import { Analytics } from "@vercel/analytics/next"
 const Layout = ({children}: {children:ReactNode}) => {
   return (
     <div>
         <Navbar />
         {children}
+        <Analytics />
     </div>
   )
 }
